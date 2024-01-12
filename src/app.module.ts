46f0a75b882cfe0bfd8d4b954aef374f80d66b25
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2024-01-11 20:20:59 -0500

    cld

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, CloudinaryModule],
   controllers: [AppController],
   providers: [AppService],
 })
