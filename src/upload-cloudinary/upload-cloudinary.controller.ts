cccbcb4b698cf70d2d6651d7010bedcf048d3108
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2024-01-12 00:14:47 -0500

    Stable version - 11

--- src/upload-cloudinary/upload-cloudinary.controller.ts
+++ src/upload-cloudinary/upload-cloudinary.controller.ts
@@ -1,15 +1,38 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  // Req,
+  // Res,
+} from '@nestjs/common';
 import { UploadCloudinaryService } from './upload-cloudinary.service';
 import { CreateUploadCloudinaryDto } from './dto/create-upload-cloudinary.dto';
 import { UpdateUploadCloudinaryDto } from './dto/update-upload-cloudinary.dto';
+import {} from //  Request,
+// Response,
+'express';
 
 @Controller('upload-cloudinary')
 export class UploadCloudinaryController {
-  constructor(private readonly uploadCloudinaryService: UploadCloudinaryService) {}
+  constructor(
+    private readonly uploadCloudinaryService: UploadCloudinaryService,
+  ) {}
 
   @Post()
-  create(@Body() createUploadCloudinaryDto: CreateUploadCloudinaryDto) {
-    return this.uploadCloudinaryService.create(createUploadCloudinaryDto);
+  create(
+    @Body() createUploadCloudinaryDto: CreateUploadCloudinaryDto,
+    // @Res() response: Response,
+  ) {
+    // create(@Req() request: Request, @Res() response: Response) {
+    // return this.uploadCloudinaryService.create(createUploadCloudinaryDto);
+    // const data = request.body;
+    // return response.status(200).send('from upload-cloudinary');
+    // return response.status(200).json({ createUploadCloudinaryDto });
+    return { aditional: 'add', createUploadCloudinaryDto };
   }
 
   @Get()
@@ -23,7 +46,10 @@ export class UploadCloudinaryController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUploadCloudinaryDto: UpdateUploadCloudinaryDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateUploadCloudinaryDto: UpdateUploadCloudinaryDto,
+  ) {
     return this.uploadCloudinaryService.update(+id, updateUploadCloudinaryDto);
   }
 
